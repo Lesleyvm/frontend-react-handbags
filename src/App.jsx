dcadbe9fa5e24af6f449b8a3d175446b750c5682
Lesleyvm <lvanmusscher@gmail.com>
2023-09-11 23:25:20 +0200

    begin opdracht 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css'
 
 function App() {
-  return (
-      <h1>Handbags & Purses</h1>
-  )
+    const logButtonText = (text) => {
+        console.log(text);
+    };
+
+    return (
+        <>
+            <h1>Handbags & Purses</h1>
+                <nav>
+                    <button type={"button"} onClick={() => logButtonText("to the collection")}>to the collection</button>
+                    <button type={"button"} onClick={() => logButtonText("shop all bags")}>shop all bags</button>
+                    <button type={"button"} disabled>pre-orders</button>
+                </nav>
+            <main></main>
+        </>
+    )
 }
 
-export default App
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+import './App.css';
+function Button() {
+    return (
+        <></>
+    )
+}
+
+export default Button;
